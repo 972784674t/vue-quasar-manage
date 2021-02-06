6e913cdab343e4072e79652dd2da4b5eaa7b7a94
ths <972784674@qq.com>
2021-02-06 18:55:04 +0800

    v1.0.7 beta

--- src/router/permissionUtils.js
+++ src/router/permissionUtils.js
@@ -2,7 +2,7 @@ import store from '../store'
 
 /**
  * <分治法> 构造符合权限的路由，将不符合权限的路由过滤
- * @param router 未过滤的路由 因为最外层有一层用来做布局的 {index} 路由，所以传的是 index[0].children
+ * @param router 未过滤的路由 因为最外层有一层用来做布局的 {index} 路由，{index} 路由不参与权限过滤，所以传的是 index[0].children
  * @param t 暂存变量
  * @returns {*} 过滤后的路由
  */
